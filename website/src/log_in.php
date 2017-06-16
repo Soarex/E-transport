0a18da7915d206941192c810b5efc4bde02c0ace
Soarex <alessio_98x@hotmail.it>
2017-06-16 18:11:13 +0200

    Added login and signup pages

--- website/src/log_in.php
+++ website/src/log_in.php
@@ -1,5 +1,43 @@
+<html>
 <?php
     include_once("database.php");
-    $d = new Database();
-    $d->connect
+    session_start();
+    class User  {
+        public $email;
+        public $password;
+        public $nome;
+        public $cognome;
+        public $recapito;
+        public $numero_documento;
+        public $id;
+    }
+
+    $database = new Database();
+    $database->connect("localhost", "root", ""); 
+    $database->selectDatabase("e_transport");
+    $result = $database->query("SELECT * FROM cliente WHERE email='".$_POST['email']."'");
+
+    if($result->num_rows == 0) {
+        echo "<script>alert('Utente non esistente'); window.location.href='log_in.html';</script>";
+        die();
+    }
+
+    $data = $result->fetch_assoc();
+    if($data['password'] != $_POST['password']) {
+        echo "<script>alert('Username o password errati'); window.location.href='log_in.html';</script>";
+        die();
+    }
+
+    $user = new User();
+    $user->email = $data['email'];
+    $user->password = $data['password'];
+    $user->nome = $data['nome'];
+    $user->cognome = $data['cognome'];
+    $user->recapito = $data['recapito'];
+    $user->numero_documento = $data['numero_documento'];
+    $user->id = $data['id'];
+
+    $_SESSION['logged_user'] = $user;
+    header("Location: http://localhost/E-transport/website/index.html");
 ?>
+</html>
--- website/src/nav_user.php
+++ website/src/nav_user.php
@@ -1,9 +1,19 @@
 <?php
     session_start();
+    class User  {
+        public $email;
+        public $password;
+        public $nome;
+        public $cognome;
+        public $recapito;
+        public $numero_documento;
+        public $id;
+    }
+    //unset($_SESSION['logged_user']);
     if(isset($_SESSION['logged_user'])) {
         echo '
-            <li><a id="user_icon"><img src="res/user_icon.png" alt="user_icon" /></a></li>
-            <li><a id="user_name">'.$_SESSION['logged_user']->nome.' '.$_SESSION['logged_user']->cognome.'</a></li>
+            <li style="float:right"><a id="user_icon"><img src="res/user_icon.png" alt="user_icon" /></a></li>
+            <li style="float:right"><a id="user_name">'.$_SESSION['logged_user']->nome.' '.$_SESSION['logged_user']->cognome.'</a></li>
         ';
     } else {
         echo ' 
